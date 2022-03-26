fb4b3353fb27346ca70713ac4a2d10525d131e25
VikashPR <vikashrajendran2806@gmail.com>
2022-03-27 00:20:04 +0530

    Feat: veusax add

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,21 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
 
 <style>
+* {
+  padding:0;
+  margin:0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuesax from 'vuesax'
 
-Vue.config.productionTip = false
+
+import 'vuesax/dist/vuesax.css'
+import 'material-icons/iconfont/material-icons.css';
+
+Vue.use(Vuesax, {
+  colors: {
+    primary:'#5b3cc4',
+    success:'rgb(23, 201, 100)',
+    danger:'rgb(242, 19, 93)',
+    warning:'rgb(255, 130, 0)',
+    dark:'rgb(36, 33, 69)'
+  }
+})
+
+
 
 new Vue({
   render: h => h(App),
