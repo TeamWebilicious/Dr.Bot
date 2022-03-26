a7a3f51f961b272ca2ef77dd1a4f9c72f25e8ad7
VikashPR <vikashrajendran2806@gmail.com>
2022-03-27 00:32:21 +0530

    Fix: vuesax add revert

--- src/main.js
+++ src/main.js
@@ -1,22 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuesax from 'vuesax'
 
-
-import 'vuesax/dist/vuesax.css'
-
-
-Vue.use(Vuesax, {
-  colors: {
-    primary:'#5b3cc4',
-    success:'rgb(23, 201, 100)',
-    danger:'rgb(242, 19, 93)',
-    warning:'rgb(255, 130, 0)',
-    dark:'rgb(36, 33, 69)'
-  }
-})
-
-
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
