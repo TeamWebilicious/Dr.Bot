46e70defba8b35b480f896324cf7c13c8b0f944a
VikashPR <vikashrajendran2806@gmail.com>
2022-03-27 02:06:31 +0530

    Feat: doc gif add Home page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-
+import LottieVuePlayer from "@lottiefiles/vue-lottie-player";
 Vue.config.productionTip = false
 
+Vue.use(LottieVuePlayer);
 new Vue({
   vuetify,
   render: h => h(App)
