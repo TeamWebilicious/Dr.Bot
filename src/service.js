7f5ede82bd9d5f456d0a6ce9f947e9a5c8484f57
Webilicious <96039545+hariMuruga@users.noreply.github.com>
2022-03-27 12:45:10 +0530

    fix

--- src/service.js
+++ src/service.js
@@ -1,9 +1,9 @@
-import {
-    // db,
-    // auth,
-    functions
-} from "./firebase";
-
+// import {
+//     // db,
+//     // auth,
+//     functions
+// } from "./firebase";
+import axios from "axios";
 export const msgService = {
     dialogflowGateway
 };
@@ -11,19 +11,57 @@ export const msgService = {
 
 async function dialogflowGateway(msg) {
 
-    const dialogflowGatewayCF = functions.httpsCallable('dialogflowGateway');
-    await dialogflowGatewayCF({
-        "sessionId": msg,
+    // const dialogflowGatewayCF = functions.httpsCallable('dialogflowGateway');
+    // await dialogflowGatewayCF({
+    //     "sessionId": msg,
+    //     "queryInput": {
+    //         "text": {
+    //             "text": "blue",
+    //             "languageCode": "en-US"
+    //         }
+    //     }
+    // })
+    console.log(msg);
+
+    const article = {
+        "sessionId": "fookk",
         "queryInput": {
             "text": {
-                "text": "blue",
+                "text": msg,
                 "languageCode": "en-US"
             }
         }
-    })
+    }
+    return await axios.post("http://localhost:5001/colombo-mail/us-central1/dialogflowGateway", article)
         .then((response) => {
-            console.log("Create order response", response);
-            return response
+
+
+            let options = []
+            if (response.data.fulfillmentMessages[1])
+                if (response.data.fulfillmentMessages[1].payload.fields.options.listValue) {
+                    console.log("listValue");
+                    let id = 1
+                    response.data.fulfillmentMessages[1].payload.fields.options.listValue.values.forEach(optionRaw => {
+                        console.log("values");
+                        const newOption = {
+                            text: optionRaw.stringValue,
+                            id: ++id
+                        }
+                        options.push(newOption)
+                    }
+                    )
+                }
+
+
+
+
+
+            const res = {
+                message: response.data.fulfillmentMessages[0].text.text[0],
+                options: options
+            }
+            console.log(res);
+            return res
         })
         .catch((err) => {
             console.error("Create order error", err.code, err.message);
